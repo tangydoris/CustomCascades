9a0a815cb64cf44356ed58906ff9f8bc099bc8f4
jhishan <jhishan@gmail.com>
2016-12-02 19:04:41 -0500

    create css file upload

--- css_app/views.py
+++ css_app/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from django.template import loader
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import CSSFile
 
 # Create your views here.
 
+@login_required
 def index(request):
-    return HttpResponse("hello there")
+	latest_css_file_list = CSSFile.objects.order_by('-created_at')[:5]
+	context = {'latest_css_file_list': latest_css_file_list}
+	if(request.method == 'POST'):
+		# DO THE POST STUFF
+		title = request.POST['title']
+		host = request.POST['host']
+		css_text = request.POST['css_text']
+		new_css_file = CSSFile(title=title, host=host, css_text=css_text, user=request.user)
+		new_css_file.save()
+	return render(request, 'css_app/index.html', context)
+
+# def post(request):
+#   if request.method == 'POST':
+#     form = PostForm(request.POST)
+#     new_post = form.save(commit=False)
+#     new_post.user = request.user
+#     new_post.pub_date = timezone.now()
+#     new_post.save()
+#     return home(request)
+#   else:
+#     form = PostForm
+#   return render(request, 'micro/post.html', {'form' : form})
+
+def detail(request, cssfile_id):
+    css_file = get_object_or_404(CSSFile, pk=cssfile_id)
+    return render(request, 'css_app/detail.html', {'css_file': css_file})
+
+
+def results(request, cssfile_id):
+	response = "You are looking at the results of cssfile %s."
+	return HttpResponse(results % cssfile_id)
+
+def vote(request, cssfile_id):
+	return HttpResponse("You're voting on cssfile %s" % cssfile_id)
