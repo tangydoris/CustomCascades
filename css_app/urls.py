4644b002d51759e1cc4ba73738f437cb4e2ed0d8
jhishan <jhishan@gmail.com>
2016-12-02 19:04:41 -0500

    create css file upload

--- css_app/urls.py
+++ css_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name="index"),
+    # ex: /css_file/5/
+	url(r'^(?P<cssfile_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /css_file/5/results/
+    url(r'^(?P<cssfile_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /css_file/5/vote/
+    url(r'^(?P<cssfile_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
