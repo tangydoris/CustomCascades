82a2f6c168227d0f101e6206d1c374af8bd1c1b7
Doris Tang <huiyao.tang@nyu.edu>
2016-12-05 22:33:20 -0500

    Working search and trending sort

--- css_app/urls.py
+++ css_app/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     # ex: /css_file/5/
 	url(r'^(?P<cssfile_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /css_file/5/results/
-    url(r'^(?P<cssfile_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /css_app/search_results/
-    url(r'^search_results/$', views.search_results, name="search_results"),
-   	url(r'^upvote/(?P<cssfile_id>[0-9]+)/$', views.upvote, name='upvote'),
-   	url(r'^remove/(?P<cssfile_id>[0-9]+)/$', views.remove, name='remove'),
-   	url(r'^save/(?P<cssfile_id>[0-9]+)/$', views.save, name='save'),
-    url('^', include('django.contrib.auth.urls')),
+  url(r'^(?P<cssfile_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /css_app/search/
+  url(r'^search/$', views.search, name="search"),
+ 	url(r'^upvote/(?P<cssfile_id>[0-9]+)/$', views.upvote, name='upvote'),
+ 	url(r'^remove/(?P<cssfile_id>[0-9]+)/$', views.remove, name='remove'),
+ 	url(r'^save/(?P<cssfile_id>[0-9]+)/$', views.save, name='save'),
+  url('^', include('django.contrib.auth.urls')),
 ]
